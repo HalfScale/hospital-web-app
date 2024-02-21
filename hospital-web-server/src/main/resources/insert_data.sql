60f08dc6c0a7f6b9380531f125269131ff315215
HalfScale <halfscale3@gmail.com>
2024-02-21 11:29:26 +0800

    fix: adjusted insert script passwords for patient and doctor

--- hospital-web-server/src/main/resources/insert_data.sql
+++ hospital-web-server/src/main/resources/insert_data.sql
@@ -17,7 +17,7 @@ SELECT 1, ins_admin.id FROM ins_admin;
 -- Data modifying statements in WITH
 WITH ins_doctor1 AS (
     INSERT INTO users (email, password, enabled, is_confirmed, user_type, deleted)
-    VALUES ('joefazer@gmail.com', '$2a$10$XH5jMGgFVfgnrDinKnxF4.gy2vgM.Ipr9dQ.2TYwYfSqm2SZd2pKW', true, true, 2, false)
+    VALUES ('joefazer@gmail.com', '$2a$10$rfN6Z9FD.lUmDLgDfgKKBOKU89rb3KFR0Kytd9hkVLTKqDz4XQGs2', true, true, 2, false)
     RETURNING id
 ), ins_doctor1_details AS (
 	INSERT INTO user_details (users_id, first_name, last_name, gender, deleted)
@@ -29,7 +29,7 @@ SELECT 2, ins_doctor1.id FROM ins_doctor1;
 
 WITH ins_doctor2 AS (
     INSERT INTO users (email, password, enabled, is_confirmed, user_type, deleted)
-    VALUES ('janedoe@gmail.com', '$2a$10$XH5jMGgFVfgnrDinKnxF4.gy2vgM.Ipr9dQ.2TYwYfSqm2SZd2pKW', true, true, 2, false)
+    VALUES ('janedoe@gmail.com', '$2a$10$rfN6Z9FD.lUmDLgDfgKKBOKU89rb3KFR0Kytd9hkVLTKqDz4XQGs2', true, true, 2, false)
     RETURNING id
 ), ins_doctor2_details AS (
 	INSERT INTO user_details (users_id, first_name, last_name, gender, deleted)
@@ -41,7 +41,7 @@ SELECT 2, ins_doctor2.id FROM ins_doctor2;
 
 WITH ins_patient1 AS (
     INSERT INTO users (email, password, enabled, is_confirmed, user_type, deleted)
-    VALUES ('patrick@gmail.com', '$2a$10$XH5jMGgFVfgnrDinKnxF4.gy2vgM.Ipr9dQ.2TYwYfSqm2SZd2pKW', true, true, 3, false)
+    VALUES ('patrick@gmail.com', '$2a$10$rfN6Z9FD.lUmDLgDfgKKBOKU89rb3KFR0Kytd9hkVLTKqDz4XQGs2', true, true, 3, false)
     RETURNING id
 ), ins_patient1_details AS (
 	INSERT INTO user_details (users_id, first_name, last_name, gender, deleted)
@@ -53,7 +53,7 @@ SELECT 3, ins_patient1.id FROM ins_patient1;
 
 WITH ins_patient2 AS (
     INSERT INTO users (email, password, enabled, is_confirmed, user_type, deleted)
-    VALUES ('monica@gmail.com', '$2a$10$XH5jMGgFVfgnrDinKnxF4.gy2vgM.Ipr9dQ.2TYwYfSqm2SZd2pKW', true, true, 3, false)
+    VALUES ('monica@gmail.com', '$2a$10$rfN6Z9FD.lUmDLgDfgKKBOKU89rb3KFR0Kytd9hkVLTKqDz4XQGs2', true, true, 3, false)
     RETURNING id
 ), ins_patient2_details AS (
 	INSERT INTO user_details (users_id, first_name, last_name, gender, deleted)
